cf1e20a29d8002a50a5f4d814b838bd27dd5c9ea
washizawa83 <washizawa83@gmail.com>
2023-11-28 22:01:57 +0900

    画面イメージ作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 import { InfomationComponent } from './components/sidebar/infomation/infomation.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MenuButtonComponent } from './components/ui/menu-button/menu-button.component';
+import { ChartComponent } from './components/dashboard/chart/chart.component';
+import { IgxFinancialChartModule } from 'igniteui-angular-charts';
+import { TradingHistoryComponent } from './components/dashboard/trading-history/trading-history.component';
+import { SignalComponent } from './components/dashboard/signal/signal.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +22,17 @@ import { MenuButtonComponent } from './components/ui/menu-button/menu-button.com
     MenuButtonComponent,
     SidebarComponent,
     InfomationComponent,
+    ChartComponent,
+    TradingHistoryComponent,
+    SignalComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    IgxFinancialChartModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
